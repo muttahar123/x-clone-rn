b4fc26d58c634b82a58412daf1dabc86c6c7795f
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-10-25 02:17:05 +0500

    user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    clerkId:{
+    type:String,
+    required:true,
+    unique:true
+},
+    email:{
+    type:String,
+    required:true,
+    unique:true
+},
+    firstName:{
+    type:String,
+    required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    profileImage:{
+        type:String,
+        required:true
+    },
+    bannerImage:{
+        type:String,
+        required:true
+    },
+    bio:{
+        type:String,
+        default:"",
+    maxlength:160
+    },
+   location:{
+    type:String,
+    default:""
+   },
+   followers:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+   }],
+    following:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+   }],
+
+},{ timestamps: true})
+
+ const User = mongoose.model("User",userSchema);
+ export default User;
