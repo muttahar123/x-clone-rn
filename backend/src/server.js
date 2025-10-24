99f17388e40a4ebfe9b15d205a09f5020ec6b806
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-10-24 22:12:30 +0500

    env setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
+import { ENV } from './config/env.js';
 
 const app = express();
 
 
-app.listen(5001, ()=> console.log('Server is running on port 5001'));
+app.listen(ENV.PORT, ()=> console.log('Server is running on port ', ENV.PORT));
