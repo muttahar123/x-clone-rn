d61f8f10a01a5fcc40f9dab9062fd4444501b5b3
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-11-02 22:26:24 +0500

    implement notification management with get and delete endpoints

--- backend/src/routes/notification.route.js
+++ backend/src/routes/notification.route.js
@@ -1,1 +1,12 @@
-    
+import express from 'express';
+import {  deleteNotification, getNotifications } from '../controllers/notification.controller.js';
+import { protectedRoute } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+//protected routes
+router.post('/', protectedRoute, getNotifications);
+router.delete('/:notificationId', protectedRoute, deleteNotification);
+
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 const app = express();
 
@@ -19,6 +21,7 @@ app.get('/' , (req,res) => {
 app.use('/api/users', userRoutes);
 app.use("/api/posts", postRoutes);
 app.use('/api/comments', commentRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 //error handling middleware
 app.use((err, req, res, next) => {
