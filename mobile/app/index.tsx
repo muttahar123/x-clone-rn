6e433898b1336042d9f926ba45668eb92a075c27
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-11-06 19:40:01 +0500

    Update welcome message

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Twitter X clone in react native</Text>
     </View>
   );
 }
