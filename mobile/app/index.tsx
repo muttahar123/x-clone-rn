e746785af8df1b68fce676ee7c080370ae75dbe2
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-12-04 01:00:05 +0500

    nativewind setup

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,16 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Twitter X clone in react native</Text>
-      
+     <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
+
