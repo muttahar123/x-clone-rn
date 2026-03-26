154d4b1a73f7b425d781b59b2e7b6ab4bc93825f
Muttahar Sheikh <itxmuttahar@gmail.com>
2026-03-26 18:38:14 +0500

    feat(auth): integrate Clerk for authentication and add auth layout
    - Added @clerk/clerk-expo and @clerk/expo dependencies to package.json
    - Created .env file for Clerk publishable key
    - Implemented AuthRoutesLayout to manage authentication state and redirects
    - Added index page for authentication with basic UI

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,15 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 import "../global.css"
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 }
